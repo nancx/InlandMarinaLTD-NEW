54cdefe7cc99b97bc0462c6aa50d4bbb667c3ddb
nancex <bubblez.me6@gmail.com>
2023-07-26 21:16:04 -0600

    Initial commit

---
+++ InlandMarinaClasses/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InlandMarinaClasses
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(15)]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30)]
+        public string City { get; set; } = string.Empty;
+
+        // Navigation property
+        [Required]
+        public virtual ICollection<Lease> Leases { get; set; } = new List<Lease>();
+    }
+}
---
+++ InlandMarinaLTD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InlandMarinaLTD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
